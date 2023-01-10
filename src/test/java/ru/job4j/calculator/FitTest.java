78780bfe04a77d7f98d7ce508c98b8de48fc8824
VitaliiNov <yaveselyi@yandex.ru>
2023-01-10 20:45:45 +0800

    1.2. Входные данные в тесте [#297750 #379219]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FitTest {
+
+    @Test
+    void manWeight() {
+
+    }
+
+    @Test
+    void womanWeight() {
+    }
+}
