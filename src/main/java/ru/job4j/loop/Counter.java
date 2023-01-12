873bc8e41b689d25f4b08467be4f57cf5a2646d0
VitaliiNov <yaveselyi@yandex.ru>
2023-01-12 17:34:32 +0800

    4.4. Вложенный оператор if else. Запутанный код [#190 #380312]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,8 +3,18 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int i = start; i <= finish ; i++) {
+        for (int i = start; i <= finish; i++) {
             sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish ; i++) {
+            if ( (i % 2) == 0) {
+                sum = sum + i;
+            }
         }
         return sum;
     }
@@ -15,5 +25,8 @@ public class Counter {
         System.out.println(sum(1, 1));
         System.out.println(sum(0, 5));
         System.out.println(sum(90, 180));
+        System.out.println(sumByEven(0, 5));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
