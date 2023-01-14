20b5c20dcf1d9dfcbd1e30fec54e79d46cf434e6
VitaliiNov <yaveselyi@yandex.ru>
2023-01-14 21:32:41 +0800

    5.4. Протеиновая диета (цикл while) [#156313]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,8 +4,8 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (ivan < nik) {
-            month ++;
             ivan *= 3;
+            month += 1;
         }
         return month;
     }
